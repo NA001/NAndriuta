2e761c139449863b798995ed7c009e13ff48545e
Nicolai Andriuta <nickKolia@gmail.com>
2019-04-11 12:45:25 +0300

    removed files

--- Transactions.rb
+++
@@ -1,9 +0,0 @@
-class Transactions
-  attr_accessor :date, :description, :amount, :account_name
-  def initialize(date, description, amount, account_name)
-    @date = date
-    @description = description
-    @amount = amount
-    @account_name = account_name
-  end
-end
