e10f3e6994c06e210dcb726186241f1c57c65884
Nicolai Andriuta <nickKolia@gmail.com>
2019-04-12 17:31:19 +0300

    new changes

--- data_retriever.rb
+++ data_retriever.rb
@@ -4,13 +4,12 @@ require 'open-uri'
 require 'watir'
 require 'pry'
 require 'json'
-require_relative "Accounts.rb"
-require_relative "Transactions.rb"
+require_relative "Account.rb"
+require_relative "Transaction.rb"
 
-Selenium::WebDriver::Chrome.driver_path="/usr/local/bin/chromedriver"
 
 class Data_Retriever
-	def initialize()
+  def initialize()
     @available_accounts = []
     @transactions_records = []
     @browser = Watir::Browser.new :chrome
@@ -33,77 +32,79 @@ class Data_Retriever
   end
 
   def gather_credit_info
-    @page.css("table[id='dashboardAccounts'] tr[id]").each do |row|
-      account_name = row.css("div.info-wrapper span").first.text
-      account_available_balance = row.css("td.ng-scope > div.text-center.cellText.ng-scope span").first.text 
-      account_currency = row.css("div.text-right.cellText.ng-scope").first.text
-      credit_accs = Accounts.new(account_name, account_available_balance, account_currency,"Credit")
-      @available_accounts << credit_accs
+    @page.css("#dashboardAccounts tr[id]").each do |row|
+    account_name = row.css("span[bo-bind='row.iban']").text
+    account_currency = row.css("span[bo-bind='row.acyAvlBal | sgCurrency']").text.to_f 
+    account_available_balance = row.css("span[bo-bind='row.ccy']").text
+    credit_accs = Account.new(account_name , account_available_balance , account_currency , "Credit")
+    @available_accounts << credit_accs
     end
   end
 
   def gather_debit_info
     @page.css("#dashStep2 tr[id]").each do |row|
-    account_name = row.css("div.info-wrapper span").last.text
-    account_currency = @browser.span(:xpath, "(//*[@id='dashboardDeposits']/tbody/tr//span[@bo-bind='row.ccy'])").text
-    account_available_balance = row.css("div.text-right.cellText.ng-scope span").first.text
-    debit_accs = Accounts.new(account_name , account_currency , account_available_balance ,"Debit" )
+    account_name = row.css("span[bo-bind='row.iban']").text
+    account_currency = row.css("span[bo-bind='row.ccy']").text
+    account_available_balance = row.css("span[bo-bind='row.acyAvlBal | sgCurrency']").text.to_f
+    debit_accs = Account.new(account_name , account_currency , account_available_balance ,"Debit" )
     @available_accounts << debit_accs
    end
   end
 
   def gather_last_transactions
-  	transactions = page.css("#step1 > td:nth-child(5) > div[class='cellText ng-scope'] > span").first.text 
-  	@tr_index = 1
-	  	while  @tr_index <= transactions.count do
-	  	transaction_date = page.css('#step1 > td:nth-child(2) > div[style="width: 6.8rem"] > span').(@tr_index).first.text
-  t	ransaction_description = page.css('#step1 > td.ng-scope.icon-two-line-col > div > div > p.ng-scope').(@tr_index).first.text + " " + page.css('#step1 > td.ng-scope.icon-two-line-col > div > div    > p:nth-child(2)').first.text
-  transaction_amount = page.css("#step1 > td[style='width: 11.2rem;']:nth-child(6) > div > span").(@tr_index).first.text
-  transaction_account_name = page.css("#step1 > td:nth-child(5) > div[class='cellText ng-scope'] > span").(@tr_index).first.text
-  tr = Transactions.new(transaction_date, transaction_description, transaction_amount, transaction_account_name)
-  @transactions_records << tr
-  @tr_index += 1
-end
-end
+	@page.css("#lastFiveTransactions tr[id='step1']").each do |transaction|
+	transaction_date = transaction.css("td:nth-child(2)").text
+    transaction_description_first_row = transaction.css("td.ng-scope.icon-two-line-col p:nth-child(1)").text
+    transaction_description_second_row = transaction.css("td.ng-scope.icon-two-line-col p:nth-child(2)").text
+    transaction_amount = transaction.css("td:nth-child(6)").text.to_f
+    transaction_currency = transaction.css("td:nth-child(6)").text.chars.last(3).join
+    transaction_account_name = transaction.css("td:nth-child(5)").text
+    transaction_data = Transaction.new(transaction_date , transaction_description_first_row + " " + transaction_description_second_row, transaction_amount , transaction_account_name, transaction_currency)
+    @transactions_records << transaction_data
+   end
+  end
+
+
 
   def link_transactions_to_accounts
     @available_accounts.each do |available_accounts|
       @transactions_records.each do |transaction|
         if(transaction.account_name == available_accounts.name)
           available_accounts.transactions << transaction
-        end
       end
     end
+  end
     
-    def transform_data_to_hash
-      @json_hash = {"Accounts" => []}
-      @available_accounts.each do |account|
-        account_hash = {
-        "name" => account.name,
-        "balance" => account.balance,
-        "currency" => account.currency,
-        "description" => account.nature,
-        "transactions" => []
-        }
+  def transform_data_to_hash
+    @json_hash = {"Accounts" => []}
+    @available_accounts.each do |account|
+    account_hash = {
+    "name" => account.name,
+    "balance" => account.balance,
+    "currency" => account.currency,
+    "description" => account.nature,
+    "transactions" => []
+    }
         
-        account.transactions.each do |transaction|
-          transaction_hash = {
-          "date" => transaction.date,
-          "description" => transaction.description,
-          "amount" => transaction.amount
-          }
+    account.transactions.each do |transaction|
+    transaction_hash = {
+    "date" => transaction.date,
+    "description" => transaction.description,
+    "amount" => transaction.amount,
+    "currency" => transaction.currency
+    }
 
-          account_hash["transactions"] << transaction_hash
-        end
-        @json_hash["Accounts"] << account_hash
-      end
+    account_hash["transactions"] << transaction_hash
     end
-
-
-    def save(path)
-      File.open(path, "w") do |file|
-        file.write(JSON.pretty_generate(@json_hash))
-      end
+    @json_hash["Accounts"] << account_hash
     end
+  end
+
+
+  def save(path)
+    File.open(path, "w") do |file|
+    file.write(JSON.pretty_generate(@json_hash))
+    end
+   end
   end
 end
--- runner.rb
+++ runner.rb
@@ -1,15 +1,16 @@
 require_relative 'Data_Retriever'
-BASE_URL = "https://my.fibank.bg/oauth2-server/login?client_id=E_BANK"
 
 class Runner
-    parser = Data_Retriever.new
-    parser.browse(BASE_URL)
-    parser.login
-    parser.parsing
-    parser.gather_credit_info
-    parser.gather_debit_info
-    parser.gather_last_transactions
-    parser.link_transactions_to_accounts
-    parser.transform_data_to_hash
-    parser.save("/Users/nicolaiandriuta/Andriuta/Parser/result.json")
+  BASE_URL = "https://my.fibank.bg/oauth2-server/login?client_id=E_BANK"
+
+  parser = Data_Retriever.new
+  parser.browse(BASE_URL)
+  parser.login
+  parser.parsing
+  parser.gather_credit_info
+  parser.gather_debit_info
+  parser.gather_last_transactions
+  parser.link_transactions_to_accounts
+  parser.transform_data_to_hash
+  parser.save("/Users/nicolaiandriuta/https:/github.com/NA001/NAndriuta.git/result.json")
 end
