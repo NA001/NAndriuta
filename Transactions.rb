b73a47e258b9acfe796abdb88138d656d8ee18de
NA001 <47428120+NA001@users.noreply.github.com>
2019-04-12 17:43:30 +0300

    Update Transactions.rb

--- Transactions.rb
+++ Transactions.rb
@@ -1,9 +1,10 @@
-class Transactions
-  attr_accessor :date, :description, :amount, :account_name
-  def initialize(date, description, amount, account_name)
+class Transaction
+  attr_accessor :date, :description, :amount, :account_name, :currency
+  def initialize(date, description, amount, account_name, currency)
     @date = date
     @description = description
     @amount = amount
     @account_name = account_name
+    @currency = currency
   end
 end
